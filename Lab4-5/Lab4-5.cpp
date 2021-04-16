cb6c7203a8364fab74c14afe74c69937df648b54
ShkulevMaksim <79005304+ShkulevMaksim@users.noreply.github.com>
2021-04-16 14:59:55 +0700

    hhhhhhhhhhh

--- Lab4-5/Lab4-5.cpp
+++ Lab4-5/Lab4-5.cpp
@@ -1,113 +1,154 @@
 ﻿#include <iostream>
+#include <fstream>
 
 using namespace std;
 
 
-struct Node {
-	int Value;
-	Node* Next, * Prev;
+struct node {
+	int value;
+	node* next, * prev;
 };
 
-class myList
+class my_list
 {
-    Node* Head, * Tail;                 //Указатели на адреса начала списка и его конца
+    node* head_, * tail_;                 //Указатели на адреса начала списка и его конца
 public:
-    myList() :Head(NULL), Tail(NULL) {};    //Инициализируем адреса как пустые
-    ~myList();                           //Прототип деструктора
-    void Print_list();                       //Прототип функции отображения списка на экране
-    void Add_item(int item);
-    void Search_item(int item);
-    void Delete_item();
+    my_list() :head_(nullptr), tail_(nullptr) {}
+    ~my_list();                           //Прототип деструктора
+    void print_list() const;                       //Прототип функции отображения списка на экране
+    void add_item(int item);
+    void search_item(int item) const;
+    void delete_item();
+    
 };
 
-myList::~myList()                           //Деструктор
+my_list::~my_list()                           //Деструктор
 {
-    while (Head)                       //Пока по адресу на начало списка что-то есть
+    while (head_)                       //Пока по адресу на начало списка что-то есть
     {
-        Tail = Head->Next;             //Резервная копия адреса следующего звена списка
-        delete Head;                   //Очистка памяти от первого звена
-        Head = Tail;                   //Смена адреса начала на адрес следующего элемента
+        tail_ = head_->next;             //Резервная копия адреса следующего звена списка
+        delete head_;                   //Очистка памяти от первого звена
+        head_ = tail_;                   //Смена адреса начала на адрес следующего элемента
     }
 }
 
-void myList::Add_item(int item){
-    Node* temp = new Node;               //Выделение памяти под новый элемент структуры
-    temp->Prev = NULL;                   //Указываем, что изначально по следующему адресу пусто
-    temp->Value = item;                         //Записываем значение в структуру
+void my_list::add_item(const int item){
+	const auto temp = new node;               //Выделение памяти под новый элемент структуры
+    temp->prev = nullptr;                   //Указываем, что изначально по следующему адресу пусто
+    temp->value = item;                         //Записываем значение в структуру
 
-    if (Head != NULL)                    //Если список не пуст
+    if (head_ != nullptr)                    //Если список не пуст
     {
-        temp->Next = Head;               //Указываем адрес на предыдущий элемент в соотв. поле
-        Head->Prev = temp;               //Указываем адрес следующего за хвостом элемента
-        Head = temp;                     //Меняем адрес хвоста
+        temp->next = head_;               //Указываем адрес на предыдущий элемент в соотв. поле
+        head_->prev = temp;               //Указываем адрес следующего за хвостом элемента
+        head_ = temp;                     //Меняем адрес хвоста
     }
     else //Если список пустой
     {
-        temp->Next = NULL;               //Предыдущий элемент указывает в пустоту
-        Head = Tail = temp;              //Голова=Хвост=тот элемент, что сейчас добавили
+        temp->next = nullptr;               //Предыдущий элемент указывает в пустоту
+        head_ = tail_ = temp;              //Голова=Хвост=тот элемент, что сейчас добавили
     }
 }
 
-void myList::Print_list() {
-    Node* temp = Tail;                   //Временный указатель на адрес последнего элемента
+void my_list::print_list() const
+{
+	node* temp = tail_;                   //Временный указатель на адрес последнего элемента
     cout << "List: ";
-    temp = Head;                       //Временно указываем на адрес первого элемента
-    while (temp != NULL)              //Пока не встретим пустое значение
+    temp = head_;                       //Временно указываем на адрес первого элемента
+    while (temp != nullptr)              //Пока не встретим пустое значение
     {
-        cout << temp->Value << " ";        //Выводим каждое считанное значение на экран
-        temp = temp->Next;             //Смена адреса на адрес следующего элемента
+        cout << temp->value << " ";        //Выводим каждое считанное значение на экран
+        temp = temp->next;             //Смена адреса на адрес следующего элемента
     }
     cout << "\n";
 }
 
-void myList::Search_item(int item) {
-    Node* temp = Head;
-    while (temp != NULL) {
-        if (temp ->Value == item)
+void my_list::search_item(const int item) const
+{
+    node* temp = head_;
+    while (temp != nullptr) {
+        if (temp ->value == item)
         {
             cout << "Found" << endl;
         }
-        temp = temp->Next;
+        temp = temp->next;
     }
 }
 
-void myList::Delete_item() {
-    if ((Tail != NULL) && (Head != Tail)) {  //если список не пустой и не состоит из 1 элемента
-        Node* temp = Tail;	                            
-        Tail = Tail->Prev;	                                
-        Tail->Next = NULL;	                                
+void my_list::delete_item() {
+    if ((tail_ != nullptr) && (head_ != tail_)) {  //если список не пустой и не состоит из 1 элемента
+        node* temp = tail_;	                            
+        tail_ = tail_->prev;	                                
+        tail_->next = nullptr;	                                
         delete temp;	                                    
-        cout << "Succsesfully deleted" << endl;
+        cout << "Successfully deleted" << endl;
     }
     else
     {
-        if ((Head == Tail) && (Head != NULL)) {  //Если всего 1 элемент
-            Head->Next = NULL;
-            Head = NULL;
-            Tail = NULL;
-            delete Head;
-            delete Tail;
-            cout << "Succsesfully deleted" << endl;
+        if ((head_ == tail_) && (head_ != nullptr)) {  //Если всего 1 элемент
+            head_->next = nullptr;
+            head_ = nullptr;
+            tail_ = nullptr;
+            delete head_;
+            delete tail_;
+            cout << "Successfully deleted" << endl;
         }
         else
         {
-            if (Head == NULL && Tail == NULL)  //Если список пустой
+            if (head_ == nullptr && tail_ == nullptr)  //Если список пустой
             {
                 cout << "Error. List is empty" << endl;
             }
         }
     }
 }
+
+void generate_file(const string& filename)
+{
+    std::ofstream my_file(filename);
+    srand(time(nullptr));
+    auto num = 0;
+    cout << "Enter number of lines: " << endl;
+    cin >> num;
+
+    for (auto i = 0; i < num; i++)
+    {
+        for (auto j = 0; j < 5; j++)
+        {
+            my_file << static_cast<int>(rand()) << "\t";
+        }
+        my_file << std::endl;
+    }
+    my_file.close();
+}
+
+void fill_list(my_list &lst, const string filename)
+{
+	  int temp;
+    std::ifstream my_file;
+    my_file.open(filename);
+    while (my_file >> temp) {
+        (&lst)->add_item(temp);
+    }
+    my_file.close();
     
-       
-    
+
+}
+
 
 
 int main()
 {
     system("CLS");
-    myList lst;
-    auto item = 0;
+    my_list lst;
+    const string filename = "C:/Users/mksh2/source/repos/Lab4-5/input.txt";
+	generate_file(filename);
+    fill_list(lst, filename);
+    lst.print_list();
+    
+
+    
+	
     cout << "Choose an option:" << endl<<endl;
     cout << "1. Add item to List" << endl;
     cout << "2. Remove last item from List" << endl;
@@ -117,41 +158,40 @@ int main()
     cout << endl;
     auto a=-1;
     auto While = true;
+	auto item = 0;
     while (While) {
         cin >> a;
         switch (a)
         {
+	        
+
         case 1:
             cout << "Enter a number to add:" << endl;
             cin >> item;
-            lst.Add_item(item);
+            lst.add_item(item);
             break;
         case 2:
-            lst.Delete_item();
+            lst.delete_item();
             break;
         case 3:
             cout << "Enter a number to find:" << endl;
             cin >> item;
-            lst.Search_item(item);
+            lst.search_item(item);
             break;
         case 4:
-            lst.Print_list();
+            lst.print_list();
             break;
         case 5:
             While = false;
 
             break;
         default:
-            cout << "Not an option" << endl;;
+            cout << "Not an option" << endl;
             While = false;
    
             break;
         }
     }
       
-   
-    //TODO сделать ввод в список из файла
-   
-
 }
 
